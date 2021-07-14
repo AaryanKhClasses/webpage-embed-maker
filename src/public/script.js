f1734896cb8207ba7e0680a07efa42960f80f7cd
AaryanKhClasses <aaryankhclasses07@gmail.com>
2021-07-14 15:24:05 +0530

    why is my preview tab not working ;(

--- src/public/script.js
+++ src/public/script.js
@@ -1,12 +1,13 @@
 // Script which runs of the "index.js" file
-function getInputValues() { // After clicking the "Submit" button    
-    // Getting the value of the elements.
-    const title = document.getElementById("input_title").value.trim()
-    const url = document.getElementById("input_url").value.trim()
-    const image = document.getElementById("input_image").value.trim()
-    const type = document.getElementById("input_type").value.trim()
-    const desc = document.getElementById("input_desc").value.trim()
 
+// Getting the value of the elements.
+const title = document.getElementById("input_title").value.trim()
+const url = document.getElementById("input_url").value.trim()
+const image = document.getElementById("input_image").value.trim()
+const type = document.getElementById("input_type").value.trim()
+const desc = document.getElementById("input_desc").value.trim()
+
+function getInputValues() { // After clicking the "Submit" button    
     // Errors
     if(title === "" || url === "" || image === "" || type === "" || desc === "") { // If any of the input fields are blank, this will get executed.
         title == url == image == type == desc == "" // This will make all the input fields blank again.
@@ -30,10 +31,38 @@ function getInputValues() { // After clicking the "Submit" button
     // }
 
     // After fixing all the errors, the final product:
+    // Makes the "Result" panel visible and shows the text
     document.getElementById("result").style.display = 'inline'
     document.getElementById("result_title").textContent = `<meta property = "og:title" content="${title}" />`
     document.getElementById("result_url").textContent = `<meta property = "og:url" content="${url}" />`
     document.getElementById("result_image").textContent = `<meta property = "og:image" content="${image}" />`
     document.getElementById("result_type").textContent = `<meta property = "og:type" content="${type}" />`
-    document.getElementById("result_desc").textContent = `<meta property = "og:description" content="${desc}" />`
+    document.getElementById("result_desc").textContent = `<meta property = "og:description" content="${desc}" />`  
+}
+
+// Previews the form.
+let titleLabel, urlLabel, imageLabel, typeLabel, descLabel // Declares the variables.
+document.getElementById("input_title").onkeyup = function() { // This will run when a letter has been typed in the "Title" input field.
+    if(title !== "") { titleLabel = title } else { titleLabel = "Title" } // If the "input title" is blank, it will preview as "Title", else it will preview as the inputted value.
+    document.getElementById("preview_title").value = titleLabel // Makes the preview "title" text same as the inputted "title" text.
+}
+
+document.getElementById("input_url").onkeyup = function() { // This will run when a letter has been typed in the "URL" input field.
+    if(url !== "") { urlLabel = url } else { urlLabel = "URL" } // If the "input URL" is blank, it will preview as "URL", else it will preview as the inputted value.
+    document.getElementById("preview_url").textContent = urlLabel // Makes the preview "URL" text same as the inputted "URL" text.
+}
+
+document.getElementById("input_image").onkeyup = function() { // This will run when a letter has been typed in the "Image" input field.
+    if(image !== "") { imageLabel = image } else { imageLabel = "Image" } // If the "input image" is blank, it will preview as "Image", else it will preview as the inputted value.
+    document.getElementById("preview_image").textContent = imageLabel // Makes the preview "image" text same as the inputted "image" text.
+}
+
+document.getElementById("input_type").onkeyup = function() { // This will run when a letter has been typed in the "type" input field.
+    if(type !== "") { typeLabel = type } else { typeLabel = "Type" } // If the "input type" is blank, it will preview as "Type", else it will preview as the inputted value.
+    document.getElementById("preview_type").textContent = typeLabel // Makes the preview "type" text same as the inputted "type" text.
+}
+
+document.getElementById("input_desc").onkeyup = function() { // This will run when a letter has been typed in the "description" input field.
+    if(desc !== "") { descLabel = desc } else { descLabel = "Description" } // If the "input description" is blank, it will preview as "Description", else it will preview as the inputted value.
+    document.getElementById("preview_desc").textContent = descLabel // Makes the preview "description" text same as the inputted "description" text.
 }
